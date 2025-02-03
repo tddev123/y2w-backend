d22f6f5ec2b96e100405ac3596cf23708d49e3cb
tddev123 <154409493+tddev123@users.noreply.github.com>
2025-02-03 05:42:58 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from app import create_app
+from flask import Flask
+from .routes.download import download_bp
 
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(download_bp, url_prefix="/api")
+    return app
+
+# Create the app instance
 app = create_app()
 
+# Run the app
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
