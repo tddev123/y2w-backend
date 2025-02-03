6a267c1ffecac302c28c203c6914227675f16949
tddev123 <154409493+tddev123@users.noreply.github.com>
2025-02-03 05:43:19 -0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,1 @@
-from flask import Flask
-from .routes.download import download_bp
 
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(download_bp, url_prefix="/api")
-    return app
